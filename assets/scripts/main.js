4d696f98348116aff94f48b1435fbd476e21048f
jinxiaokazama123 <jinxiaokazama@gmail.com>
2024-08-02 20:01:07 +0800

    first commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,12 @@
+const body = document.body
+
+const div = document.querySelector('div')
+const spanHi = document.querySelector('#hi')
+const spanBye = document.querySelector('#bye')
+
+spanHi.classList.add("new-class")
+spanHi.classList.remove("new-class")
+spanHi.classList.toggle("new-class")
+spanHi.classList.toggle("hi1")
+
+spanBye
